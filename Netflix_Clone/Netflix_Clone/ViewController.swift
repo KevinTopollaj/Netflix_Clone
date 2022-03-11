d5958203bed35662c0bb9bbbd80a4f31f899ccf5
Kevin Topollaj <kevintopollaj@gmail.com>
2022-03-11 23:53:03 +0100

    new Xcode project

---
+++ Netflix_Clone/Netflix_Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix_Clone
+//
+//  Created by Kevin Topollaj on 11.3.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
