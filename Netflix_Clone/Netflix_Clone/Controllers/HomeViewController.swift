53adf4a90d77f33ddb80031d89e7996e4ee79ca4
Kevin Topollaj <kevintopollaj@gmail.com>
2022-03-12 00:41:02 +0100

    Create Home TableView

--- Netflix_Clone/Netflix_Clone/Controllers/HomeViewController.swift
+++ Netflix_Clone/Netflix_Clone/Controllers/HomeViewController.swift
@@ -9,10 +9,49 @@ import UIKit
 
 class HomeViewController: UIViewController {
   
+  private let homeFeedTableView: UITableView = {
+    let table = UITableView()
+    table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    return table
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
     view.backgroundColor = .systemBackground
+    view.addSubview(homeFeedTableView)
+    
+    homeFeedTableView.dataSource = self
+    homeFeedTableView.delegate = self
   }
   
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    
+    homeFeedTableView.frame = view.bounds
+  }
+  
+}
+
+extension HomeViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 20
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    cell.textLabel?.text = "Hello!"
+    cell.backgroundColor = .systemCyan
+    return cell
+  }
+}
+
+extension HomeViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 200
+  }
+  
+  func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    return 40
+  }
 }
